7dfeede101fe9d9bbbf53028bc8eda8d3b8d2a04
Abdelmoumen.m <Abdelmoumen.m@DESKTOP-T78TDJH>
2020-03-10 15:11:58 +0000

    Abdel - PAge Prestige

---
+++ Test Suites/TS-Abdel.ts
@@ -0,0 +1,19 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<TestSuiteEntity>
+   <description></description>
+   <name>TS-Abdel</name>
+   <tag></tag>
+   <isRerun>false</isRerun>
+   <mailRecipient></mailRecipient>
+   <numberOfRerun>0</numberOfRerun>
+   <pageLoadTimeout>30</pageLoadTimeout>
+   <pageLoadTimeoutDefault>true</pageLoadTimeoutDefault>
+   <rerunFailedTestCasesOnly>false</rerunFailedTestCasesOnly>
+   <testSuiteGuid>56739308-427e-42ed-b081-7341ca8e51cc</testSuiteGuid>
+   <testCaseLink>
+      <guid>032e930f-d479-41e6-a0c5-2c1f83cc34d8</guid>
+      <isReuseDriver>false</isReuseDriver>
+      <isRun>true</isRun>
+      <testCaseId>Test Cases/TC-Abdel</testCaseId>
+   </testCaseLink>
+</TestSuiteEntity>
