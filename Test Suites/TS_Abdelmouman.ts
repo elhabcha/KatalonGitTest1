5ee954579b5debab5061f130892d5e87997b20d1
Abdelmoumen.m <Abdelmoumen.m@DESKTOP-T78TDJH>
2020-03-10 14:38:55 +0000

    Page Prestige

---
+++ Test Suites/TS_Abdelmouman.ts
@@ -0,0 +1,19 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<TestSuiteEntity>
+   <description></description>
+   <name>TS_Abdelmouman</name>
+   <tag></tag>
+   <isRerun>false</isRerun>
+   <mailRecipient></mailRecipient>
+   <numberOfRerun>0</numberOfRerun>
+   <pageLoadTimeout>30</pageLoadTimeout>
+   <pageLoadTimeoutDefault>true</pageLoadTimeoutDefault>
+   <rerunFailedTestCasesOnly>false</rerunFailedTestCasesOnly>
+   <testSuiteGuid>72e9ef48-f8df-43a1-a459-4b4cab09f3c4</testSuiteGuid>
+   <testCaseLink>
+      <guid>0e42d8fd-d02c-4d53-a10b-599fe492bcb0</guid>
+      <isReuseDriver>false</isReuseDriver>
+      <isRun>true</isRun>
+      <testCaseId>Test Cases/TC_Abdelmouman</testCaseId>
+   </testCaseLink>
+</TestSuiteEntity>
